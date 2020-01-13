162f909daff9a6d92f0fa70d83dc373f0b522be4
Mardy A. dela Cruz <mardy.codev@gmail.com>
2020-01-13 14:36:55 +0800

    Login by API

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,36 @@ use Illuminate\Http\Request;
 |
 */
 
+
+
+Route::any('login', function (Request $request) {
+    $client = new \GuzzleHttp\Client(['timeout'  => 5]);
+
+    $response = $client->post(url('oauth/token'), [
+        'form_params' => [
+            'grant_type' => 'password',
+            'client_id' => env('OAUTH_CLIENT_ID'),
+            'client_secret' => env('OAUTH_CLIENT_SECRET'),
+//            'username' => $request->username,
+//            'password' => $request->password,
+            'username' => 'admin@admin.com',
+            'password' => 'admin',
+            'scope' => '*',
+        ],
+    ]);
+
+    // dd($response->getBody());
+
+    return response()->json(json_decode((string) $response->getBody(), true));
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('/user-test', function (Request $request) {
+    return response()->json(
+        ['name' => 'mardy', 'email' => 'mardy.codev@gmail.com'
+        ]);
 });
